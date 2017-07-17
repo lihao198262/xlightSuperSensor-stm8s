37b759c60f81a0951142e0868bcfa3cbd7c3b4d8
qiaoli.zhang <qiaoli.zhang@innonetwork.com>
2017-07-17 14:10:17 +0800

    config multiple packet add buffer

--- Project/src/main.c
+++ Project/src/main.c
@@ -614,7 +614,8 @@ int main( void ) {
     // IRQ
     NRF2401_EnableIRQ();
     // Must establish connection firstly
-    SayHelloToDevice(TRUE);
+    SayHelloToDevice(TRUE);  
+
     while (mStatus == SYS_RUNNING) {
       
       // Feed the Watchdog
@@ -754,7 +755,7 @@ int main( void ) {
             Msg_Relay_KeyMap(NODEID_GATEWAY);
           }
         }
-      } // End of if( gConfig.state )
+      } // End of if( gConfig.state )  
       
       // Send message if ready
       SendMyMessage();
@@ -810,16 +811,12 @@ void tmrProcess() {
   if(relay_loop_tick < 5000) relay_loop_tick++;
   //////zql add for relay key//////////////
 #endif  
- 
-  ////////////rfscanner process///////////////////////////////
-  ///// zql add for multiple config block ////////////////////
-  if(!bMsgReady) MsgScanner_ConfigAck(0,0,FALSE);
-  ///// zql add for multiple config block ////////////////////    
-  // Send message if ready
-  SendMyMessage(); 
-  // Save Config if Changed
-  SaveConfig();
-  ////////////rfscanner process///////////////////////////////
+  
+    ////////////rfscanner process///////////////////////////////
+    ProcessOutputCfgMsg(); 
+    // Save Config if Changed
+    SaveConfig();
+    ////////////rfscanner process///////////////////////////////
 }
 
 INTERRUPT_HANDLER(EXTI_PORTC_IRQHandler, 5) {
